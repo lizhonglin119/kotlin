8f30035e45c5ef1b803eacc99a08dc29ac6b341f
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-10-07 16:01:02 +0300

    ++ protected & internal

--- compiler/testData/diagnostics/tests/exposed/internal.kt
+++ compiler/testData/diagnostics/tests/exposed/internal.kt
@@ -7,9 +7,17 @@ internal open class Your: My() {
 }
 
 // error, public from internal
-class His: <!EXPOSED_SUPER_CLASS!>Your()<!> {
+open class His: <!EXPOSED_SUPER_CLASS!>Your()<!> {
+    protected open class Inner
     // error, public from internal
     <!EXPOSED_PROPERTY_TYPE!>val x = My()<!>
     // valid, private from internal
-    private fun bar() = My()     
+    private fun bar() = My()
+    // error, protected from internal
+    protected fun <!EXPOSED_FUNCTION_RETURN_TYPE!>baz<!>() = Your()
+}
+
+internal class Their: His() {
+    // error, effectively internal from protected
+    class InnerDerived: <!EXPOSED_SUPER_CLASS!>His.Inner()<!>
 }
