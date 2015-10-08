73d2dcff6d0515a62561214ef2ce49733fa71dd2
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-10-08 19:41:08 +0300

    Do not put inapplicable annotations on getter

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/DescriptorResolver.java
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/DescriptorResolver.java
@@ -1057,7 +1057,6 @@ public class DescriptorResolver {
         if (getter != null) {
             Annotations getterAnnotations = new CompositeAnnotations(CollectionsKt.listOf(
                     annotationSplitter.getAnnotationsForTarget(PROPERTY_GETTER),
-                    annotationSplitter.getOtherAnnotations(),
                     annotationResolver.resolveAnnotationsWithoutArguments(scope, getter.getModifierList(), trace)));
 
             JetType outType = propertyDescriptor.getType();
