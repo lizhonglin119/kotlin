11cf9755c1737722b1220a0fcf510c242be93b45
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-10-10 04:03:54 +0300

    ~ minor: add tests and link to JVMS

--- compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/platform/JvmPlatformConfigurator.kt
+++ compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/platform/JvmPlatformConfigurator.kt
@@ -520,6 +520,7 @@ public class JavaNullabilityWarningsChecker : AdditionalTypeChecker {
 
 class JvmSimpleNameBacktickChecker : IdentifierChecker {
     companion object {
+        // See The Java Virtual Machine Specification, section 4.7.9.1 https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.7.9.1
         private val CHARS = setOf('.', ';', '[', ']', '/', '<', '>', ':', '\\')
     }
 
